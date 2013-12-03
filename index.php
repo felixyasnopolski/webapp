df15dbeb306cbe0282937c800dfa0d7bf0ab055f
Felix Yasnopolski <feilikesi32@gmail.com>
2013-12-03 15:54:25 +0500

    all ok:)

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
-<form action="index.php" method="POST">
-Login:<input type="text" name="login">
-Passw:<input type="password" name="pass">	
-<input type="submit">
-</form>
 <?php
-  /*$con=mysqli_connect("localhost","root","1765160","webdev");
+$con = mysqli_connect("localhost", "root", "1765160", "webdev");
 
 // Check connection
   if (mysqli_connect_errno())
@@ -15,26 +10,19 @@ Passw:<input type="password" name="pass">
   $query = "SELECT * FROM student";
   $result = mysqli_query($con,$query);
 
-  echo "<table border='1'>
-	<tr>
-	<th>Id</th>
-	<th>FIO</th>
-	<th>Mark</th>
-  </tr>";
-
-  while($row = mysqli_fetch_array($result))
-  {
-  
-  echo "<tr>";
-  echo "<td>" . $row['id'] . "</td>";
-  echo "<td>" . $row['fio'] . "</td>";
-  echo "<td>" . $row['mark'] . "</td>";
-  echo "</tr>";
-  }
-  mysqli_close($con);*/
-if (isset($_POST['login']) && isset($_POST['pass'])) {
-	# code...
-	echo "<p>".$_POST['login'].":".$_POST['pass'];
-    print($)
+echo "<h2>List of students</h2>
+   <p> If you want to get additional info - click the link :)
+   </p>";
+echo "<table>";
+while ($row = mysqli_fetch_array($result)) {
+    echo "<tr>";
+    echo '<td><a href="student.php?id=' . $row['id'] . '">' . $row['fio'] . '</a> </td><td>  <a href="remove.php?id=' . $row['id'] . '"> remove</a></td>';
+    echo "</tr>";
 }
+echo "</table>";
+echo "<p>Admin section:
+    <a href=\"add.php\"> add student</a> </p>";
+
+mysqli_close($con);
+
 ?>
